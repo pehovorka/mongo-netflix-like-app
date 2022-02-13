f4450bdd9f488596c1a2bb2ddb884302bf5b5070
Petr Hovorka <pehovorka@gmail.com>
2022-02-13 13:49:16 +0000

    ✨ Add movie detail page

--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -1,7 +1,28 @@
-function MovieDetail(props) {
-  return <div>Movie Detail</div>;
-}
+import { useParams, useNavigate } from "react-router-dom";
+import DebugInfo from "../components/DebugInfo";
+import { FIND_TYPES, useFind } from "../hooks/useFind";
+function MovieDetail() {
+  const { slug } = useParams();
+  const navigate = useNavigate();
 
-MovieDetail.propTypes = {};
+  const req = {
+    collectionName: "movies",
+    query: { slug: slug },
+    type: FIND_TYPES.FIND_ONE,
+  };
+  const { data: movie, loading, error } = useFind(req);
+
+  if (loading) return "Loading...";
+  if (error) return error.toString();
+
+  return (
+    <>
+      <button onClick={() => navigate(-1)}>← go back</button>
+
+      <h1>{movie?.name}</h1>
+      <DebugInfo req={req} res={movie} />
+    </>
+  );
+}
 
 export default MovieDetail;
--- src/pages/MoviesList.jsx
+++ src/pages/MoviesList.jsx
@@ -1,5 +1,6 @@
 import { FIND_TYPES, useFind } from "../hooks/useFind";
 import DebugInfo from "../components/DebugInfo";
+import { Link } from "react-router-dom";
 
 function MoviesList() {
   const req = { collectionName: "movies", query: {}, type: FIND_TYPES.FIND };
@@ -8,7 +9,21 @@ function MoviesList() {
   if (loading) return "Loading...";
   if (error) return error.toString();
 
-  return <DebugInfo req={req} res={movies} />;
+  return (
+    <>
+      <h1>Movies</h1>
+      {movies && (
+        <ul>
+          {movies.map((movie) => (
+            <li key={movie._id}>
+              <Link to={movie.slug}>{movie.name}</Link>
+            </li>
+          ))}
+        </ul>
+      )}
+      <DebugInfo req={req} res={movies} />
+    </>
+  );
 }
 
 export default MoviesList;
